d9632ad201372691a1442eb97c47fc7869f7f748
salimmanji <salim@drifttech.io>
2022-06-19 16:49:31 -0600

    finished planets project

---
+++ PLANETS-PROJECT/index.js
@@ -0,0 +1,30 @@
+const { parse } = require('csv-parse');
+const fs = require('fs');
+
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
+
+fs.createReadStream('kepler_data.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true
+    })) // allows for return of JSON objects
+    .on('data', (data) => {
+        if (isHabitablePlanet(data))
+        habitablePlanets.push(data);
+    })
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on('end', () => {
+        //console.log(habitablePlanets);
+        console.log(`${habitablePlanets.length} planets found!`);
+        console.log(habitablePlanets.map((p) => {
+            return p['kepler_name'];
+        }));
+    })
