4fc154bd38629bbc84d04cbc31fbcb9aeeaef622
salimmanji <salim@drifttech.io>
2022-06-25 14:41:26 -0600

    NASA-PROJECT scripts

--- NASA-PROJECT/server/src/models/planets.model.js
+++ NASA-PROJECT/server/src/models/planets.model.js
@@ -1,3 +1,55 @@
-const planets = [];
+const { rejects } = require('assert');
+const fs = require('fs');
+const path = require('path');
+const { parse } = require('csv-parse');
 
-module.exports = planets;
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+}
+
+/*
+const promise = new Promise((resolve, reject) => {
+    resolve(42);
+});
+promise.then((result) => {
+ Do something with result.
+});
+const result = await promise; //Async code, promise first resolves, then continues
+console.log(result);
+*/
+
+function loadPlanetsData() {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+            .pipe(parse({
+                comment: '#',
+                columns: true
+            })) // allows for return of JSON objects
+            .on('data', (data) => {
+                if (isHabitablePlanet(data))
+                    habitablePlanets.push(data);
+            })
+            .on('error', (err) => {
+                console.log(err);
+                reject(err);
+            })
+            .on('end', () => {
+                //console.log(habitablePlanets);
+                // console.log(habitablePlanets.map((p) => {
+                //     return p['kepler_name'];
+                // }));
+                console.log(`${habitablePlanets.length} planets found!`);
+                resolve(); //don't need to resolve anything, we are sending back the habitable planets array in export after promise is resolved.
+            });
+    });
+}
+
+
+module.exports = {
+    loadPlanetsData,
+    planets: habitablePlanets,
+}
--- NASA-PROJECT/server/src/server.js
+++ NASA-PROJECT/server/src/server.js
@@ -6,10 +6,18 @@ const http = require('http'); // Node built in http request object
 
 const app = require('./app')
 
+const { loadPlanetsData } = require('./models/planets.model')
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app); //pass express into it. Any middleware will also be passed along
 
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
-});
+async function startServer() {
+    await loadPlanetsData();
+
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`);
+    });
+}
+
+startServer(); // no code relies on start server function.
