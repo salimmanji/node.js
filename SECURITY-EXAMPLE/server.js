457c08fd36c2716e23aeed9c06b1b56614147437
salimmanji <salim@drifttech.io>
2022-08-04 18:56:25 -0600

    created keys and self signed cert

--- SECURITY-EXAMPLE/server.js
+++ SECURITY-EXAMPLE/server.js
@@ -1,9 +1,15 @@
+
+const fs = require('fs');
 const path = require('path');
+const https = require('https');
 const express = require('express');
+const helmet = require('helmet');
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(helmet());
 
 app.get('/secret', (req, res) => {
     return res.send('Your secret value is 42!');
@@ -13,6 +19,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+https.createServer({
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem"),
+}, app).listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
