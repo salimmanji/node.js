1d5742e949a44db96c2cd395d7815a7e6dacc0a8
salimmanji <salim@drifttech.io>
2022-08-06 18:06:10 -0600

    oath2 1st part

--- SECURITY-EXAMPLE/server.js
+++ SECURITY-EXAMPLE/server.js
@@ -1,19 +1,57 @@
-
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
 const express = require('express');
 const helmet = require('helmet');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
+
+require('dotenv').config();
 
 const PORT = 3000;
+
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+
+const AUTH_OPTIONS = {
+    callbackURL: '/auth/google/callback',
+    clientID: config.clientID,
+    clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+    console.log(`Google profile`, profile);
+    done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
 const app = express();
 
 app.use(helmet());
+app.use(passport.initialize());
 
-app.get('/secret', (req, res) => {
+function checkLoggedIn(req, res, next) {
+    const isLoggedIn = true; //TODO
+    if(!isLoggedIn) { //If user is not logged in, send a 401 status and error message
+        return res.status(401).json({
+            error: 'You must login!',
+        });
+    }
+    next(); //if user is logged in, call the next() function to allow for access to other endpoints
+}
+
+app.get('/auth/google', (req, res) => {});
+
+app.get('/auth/google/callback', (req, res) => {});
+
+app.get('/auth/logout', (req, res) => {});
+
+app.get('/secret', checkLoggedIn, (req, res) => {
     return res.send('Your secret value is 42!');
-})
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
