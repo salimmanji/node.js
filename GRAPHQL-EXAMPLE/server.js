d18adc4ae59fe893b770be247ab227148779e8f9
salimmanji <salim@drifttech.io>
2022-08-12 17:27:39 -0600

    completed graphQL with Apollo server

--- GRAPHQL-EXAMPLE/server.js
+++ GRAPHQL-EXAMPLE/server.js
@@ -1,28 +1,31 @@
 const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
 
 const { loadFilesSync } = require('@graphql-tools/load-files');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
-const { parseConstValue } = require('graphql');
 
 const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'));
 const resolversArray = loadFilesSync(path.join(__dirname, '**/*.resolvers.js'));
 
-const schema = makeExecutableSchema({
-    typeDefs: typesArray,
-    resolvers: resolversArray,
-});
+async function startApolloServer() {
+    const app = express();
 
-const app = express();
+    const schema = makeExecutableSchema({
+        typeDefs: typesArray,
+        resolvers: resolversArray,
+    });
 
-app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    graphiql: true,
-}));
+    const server = new ApolloServer({
+        schema
+    });
 
-const PORT = 3000;
+    await server.start();
+    server.applyMiddleware({ app, path: '/graphql' });
 
-app.listen(PORT, () => {
-    console.log('Running GraphQL server... ')
-});
+    app.listen(3000, () => {
+        console.log('Running GraphQL server... ')
+    });
+}
+
+startApolloServer();
