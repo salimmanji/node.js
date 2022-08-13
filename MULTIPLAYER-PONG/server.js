a3c8788de0474ea53d1a67c7559e8829cb9bda67
salimmanji <salim@drifttech.io>
2022-08-13 16:34:54 -0600

    post express

--- MULTIPLAYER-PONG/server.js
+++ MULTIPLAYER-PONG/server.js
@@ -1,39 +1,19 @@
-const server = require('http').createServer();
-const io = require('socket.io')(server, {
+const http = require('http');
+const io = require('socket.io');
+
+const apiServer = require('./api');
+const httpServer = http.createServer(apiServer);
+const socketServer = io(httpServer, {
     cors: {
       origin: '*',
       methods: ['GET', 'POST']
     }
   });
 
-const PORT = 3000;
+const sockets = require('./sockets');
 
-server.listen(PORT);
+const PORT = 3000;
+httpServer.listen(PORT);
 console.log(`Listening on port ${PORT}...`);
 
-let readyPlayerCount = 0;
-
-io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);
-
-  socket.on('ready', () => {
-    console.log('Player ready ', socket.id);
-    readyPlayerCount++;
-
-    if (readyPlayerCount % 2 === 0) {
-        io.emit('startGame', socket.id);
-    }
-  });
-
-  socket.on('paddleMove', (paddleData) => {
-    socket.broadcast.emit('paddleMove', paddleData);
-  });
-
-  socket.on('ballMove', (ballData) => {
-    socket.broadcast.emit('ballMove', ballData);
-  });
-
-  socket.on('disconnect', (reason) => {
-    console.log(`Client ${socket.id} disconnected: ${reason}`);
-  });
-}); 
+sockets.listen(socketServer);
