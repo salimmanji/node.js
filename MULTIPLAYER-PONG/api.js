338409019cf20a807fc44e84d4b3cab20d3eed3a
salimmanji <salim@drifttech.io>
2022-08-13 22:24:34 -0600

    finished pong

--- MULTIPLAYER-PONG/api.js
+++ MULTIPLAYER-PONG/api.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const api = express();
 
-api.use(express.static(path.join(__dirname, "public")));
+api.use(express.static(path.join(__dirname, 'public')));
 
 api.use('/', express.static('index.html'));
 
-module.exports = api
+module.exports = api;
